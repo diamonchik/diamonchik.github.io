936943ca6d87a0b5e8aa9472b6455202b89e4b8f
diamonchik <59843455+diamonchik@users.noreply.github.com>
2021-02-22 00:53:03 +0100

    Update V2 add Portfolio+new_logo

--- js/popup.js
+++ js/popup.js
@@ -8,3 +8,16 @@ $('.close-popup').click(function() {
     $('.popup-bg').fadeOut(800);
     $('html').removeClass('no-scroll');
 });
+
+
+
+$('.open-web_1').click(function(e) {
+    e.preventDefault();
+    $('.web_1-bg').fadeIn(800);
+    $('html').addClass('no-scroll');
+});
+
+$('.close-web_1').click(function() {
+    $('.web_1-bg').fadeOut(800);
+    $('html').removeClass('no-scroll');
+});
